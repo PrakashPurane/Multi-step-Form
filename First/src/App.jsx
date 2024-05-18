299463fe7ef02f159b8a7de6318af0d68ffac04c
PrakashPurane <chhanprakash@gmail.com>
2024-05-18 00:56:56 -0700

    working on left container

---
+++ First/src/App.jsx
@@ -0,0 +1,67 @@
+import "./App.css";
+
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="left-container">
+          <div className="left-first">
+            <div className="left-listing">
+              <div className="button">
+                <button>1</button>
+              </div>
+              <div className="details">
+                <p>STEP 1</p>
+                <h1>YOUR INFO</h1>
+              </div>
+            </div>
+
+            <div className="left-second">
+            <div className="left-listing">
+              <div className="button">
+                <button>2</button>
+              </div>
+              <div className="details">
+                <p>STEP 2</p>
+                <h1>SELECT PLAN</h1>
+              </div>
+            </div>
+
+            <div className="left-third">
+            <div className="left-listing">
+              <div className="button">
+                <button>3</button>
+              </div>
+              <div className="details">
+                <p>STEP 3</p>
+                <h1>ADD-ONS</h1>
+              </div>
+            </div>
+
+            <div className="left-fourth">
+            <div className="left-listing">
+              <div className="button">
+                <button>4</button>
+              </div>
+              <div className="details">
+                <p>STEP 4</p>
+                <h1>SUMMARY</h1>
+              </div>
+            </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+
+          <div className="right-container">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident alias consectetur commodi labore laborum unde iusto, fugiat asperiores, quod voluptas libero. Accusantium, obcaecati.
+          </div>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
